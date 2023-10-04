0eef393d93e58031eae6d4e2e5dc12a7121e6e41
michael <m@ciccarel.li>
2023-10-04 13:21:54 -0400

    add clsx lib and update hover states

--- src/app/projects.js
+++ src/app/projects.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
+import cx from 'clsx';
 
 export default function Projects({ items }) {
   const [active, setActive] = useState(null);
@@ -51,7 +52,10 @@ export default function Projects({ items }) {
                 height="24"
                 viewBox="0 0 14 24"
                 fill="none"
-                className={item.id === 'oa' ? 'md:translate-y-6' : ''}>
+                className={cx('transition-transform', {
+                  'translate-x-2': active === item.id,
+                  'md:translate-y-6': item.id === 'od'
+                })}>
                 <path
                   d="M11 10.5C10.1715 10.5 9.49997 11.1716 9.49997 12C9.49997 12.8284 10.1715 13.5 11 13.5V10.5ZM13.0607 13.0607C13.6464 12.4749 13.6464 11.5251 13.0607 10.9393L3.51472 1.3934C2.92893 0.807611 1.97918 0.807611 1.3934 1.3934C0.807611 1.97919 0.807611 2.92893 1.3934 3.51472L9.87868 12L1.3934 20.4853C0.807611 21.0711 0.807611 22.0208 1.3934 22.6066C1.97918 23.1924 2.92893 23.1924 3.51472 22.6066L13.0607 13.0607ZM11 13.5H12V10.5H11V13.5Z"
                   fill="#757C8D"
@@ -59,7 +63,7 @@ export default function Projects({ items }) {
               </svg>
             </a>
           </div>
-          <p>{item.description}</p>
+          <p className={cx('transition-colors', { 'text-[#ABAFBA]': active === item.id })}>{item.description}</p>
         </div>
       ))}
     </div>
