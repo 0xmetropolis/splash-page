a5af62794542693192f1e026a8bc2b6c02fff22d
michael <m@ciccarel.li>
2023-10-04 13:28:16 -0400

    adjust arrow on oa, fix 500 errors

--- src/app/projects.js
+++ src/app/projects.js
@@ -21,7 +21,7 @@ export default function Projects({ items }) {
         break;
     }
 
-    return (
+    return id && src !== '' ? (
       <Image
         src={src}
         alt=""
@@ -31,6 +31,8 @@ export default function Projects({ items }) {
         className="max-w-[250px] md:max-w-none w-full h-auto"
         priority
       />
+    ) : (
+      <></>
     );
   };
 
@@ -54,7 +56,7 @@ export default function Projects({ items }) {
                 fill="none"
                 className={cx('transition-transform', {
                   'translate-x-2': active === item.id,
-                  'md:translate-y-6': item.id === 'od'
+                  'md:translate-y-6': item.id === 'oa'
                 })}>
                 <path
                   d="M11 10.5C10.1715 10.5 9.49997 11.1716 9.49997 12C9.49997 12.8284 10.1715 13.5 11 13.5V10.5ZM13.0607 13.0607C13.6464 12.4749 13.6464 11.5251 13.0607 10.9393L3.51472 1.3934C2.92893 0.807611 1.97918 0.807611 1.3934 1.3934C0.807611 1.97919 0.807611 2.92893 1.3934 3.51472L9.87868 12L1.3934 20.4853C0.807611 21.0711 0.807611 22.0208 1.3934 22.6066C1.97918 23.1924 2.92893 23.1924 3.51472 22.6066L13.0607 13.0607ZM11 13.5H12V10.5H11V13.5Z"
